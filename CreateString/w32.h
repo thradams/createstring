d03e92f727a52eb48ea864000984937c97c0b8cf
thradams <thiago.adams@gmail.com>
2023-07-18 16:17:21 -0300

    created w32.c and w32.h

---
+++ CreateString/w32.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <Windows.h>
+
+DWORD GetStyle(HWND hWnd);
+BOOL CenterWindow(HWND hWnd, HWND hWndCenter);
+
+INT_PTR ShowDialog(HINSTANCE hInstance,
+                   DWORD dlgID,
+                   void* p,
+                   HWND hParent,
+                   DLGPROC lpDialogFunc);
+
+BOOL IsWindowsXPOrGreater();
